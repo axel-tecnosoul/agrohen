3cf6a3cd3c522ee84e1f053b4d6fd47fb8f73ffc
Axel Britzius <axelbritzius@gmail.com>
2024-06-12 09:27:43 -0300

    corrigiendo problema que no trae datos de algunas cargas

--- models/administrar_cargas.php
+++ models/administrar_cargas.php
@@ -139,9 +139,14 @@ class cargas{
   public function getDatosCarga($id_carga){
     $datosCarga = array();
 
-    $queryCarga = "SELECT c.id AS id_carga, id_proveedor_default as id_proveedor, pr.nombre as proveedor, fecha, id_origen, o.nombre AS origen, id_chofer, datos_adicionales_chofer, ch.nombre AS chofer,IF(c.fecha_hora_despacho IS NULL,'No','Si') AS despachado,c.fecha_hora_despacho,c.id_usuario,u.usuario FROM cargas c INNER JOIN origenes o ON c.id_origen=o.id INNER JOIN proveedores pr ON c.id_proveedor_default = pr.id INNER JOIN choferes ch ON c.id_chofer=ch.id INNER JOIN usuarios u ON c.id_usuario=u.id WHERE c.id = $id_carga";
+    $queryCarga = "SELECT c.id AS id_carga, id_proveedor_default as id_proveedor, pr.nombre as proveedor, fecha, id_origen, o.nombre AS origen, id_chofer, datos_adicionales_chofer, ch.nombre AS chofer,IF(c.fecha_hora_despacho IS NULL,'No','Si') AS despachado,c.fecha_hora_despacho,c.id_usuario,u.usuario FROM cargas c INNER JOIN origenes o ON c.id_origen=o.id LEFT JOIN proveedores pr ON c.id_proveedor_default = pr.id INNER JOIN choferes ch ON c.id_chofer=ch.id INNER JOIN usuarios u ON c.id_usuario=u.id WHERE c.id = $id_carga";
+    //echo $queryCarga;
     $getCarga = $this->conexion->consultaRetorno($queryCarga);
     $rowCarga = $getCarga->fetch_array();
+    $mensajeError=$this->conexion->conectar->error;
+    if($mensajeError!=""){
+      echo $mensajeError;
+    }
 
     $fecha_hora_despacho="(La carga aún no fue despachada)";
     if($rowCarga['fecha_hora_despacho']){
@@ -167,7 +172,6 @@ class cargas{
     return json_encode($datosCarga);
   }
 
-  
   public function traerDatosVerDetalleCarga($id_carga) {
     $datosCargaJson = $this->getDatosCarga($id_carga); 
     
@@ -222,7 +226,7 @@ class cargas{
     // Codificar todo el array en JSON y enviarlo como respuesta
     return json_encode($datosNecesarios);
   }
-              
+
   public function traerCargas(){
     $sqltraerCargas = "SELECT c.id AS id_carga,c.fecha,c.id_origen,o.nombre AS origen,c.id_chofer,ch.nombre AS chofer,c.datos_adicionales_chofer,total_bultos,total_kilos,total_monto,IF(c.fecha_hora_despacho IS NULL,'No','Si') AS despachado,c.fecha_hora_despacho,c.id_usuario,u.usuario,c.anulado FROM cargas c INNER JOIN choferes ch ON c.id_chofer=ch.id INNER JOIN origenes o ON c.id_origen=o.id INNER JOIN usuarios u ON c.id_usuario=u.id WHERE 1";
     $traerCargas = $this->conexion->consultaRetorno($sqltraerCargas);
